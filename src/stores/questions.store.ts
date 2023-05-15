5f04e4072c0ac00bb5e09afcee36ba54488df0fc
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2023-05-15 03:01:39 +0200

    fix: do not persist questions

--- src/stores/questions.store.ts
+++ src/stores/questions.store.ts
@@ -1,8 +1,7 @@
 import { QuestionsService } from "@/services/questions.service";
-import { ICookieAuthKeys } from "@/types/auth.types";
 import { IAnswer } from "@/types/question.types";
 import { create } from "zustand";
-import { devtools, persist } from "zustand/middleware";
+import { devtools } from "zustand/middleware";
 
 interface QuestionsState {
   questions?: IAnswer[];
@@ -12,29 +11,22 @@ interface QuestionsState {
 }
 
 export const useQuestionsStore = create(
-  devtools(
-    persist<QuestionsState>(
-      (set, _get) => ({
-        fetchQuestions: async () => {
-          const service = new QuestionsService();
-          const questions = await service.fetchQuestions();
+  devtools<QuestionsState>((set) => ({
+    fetchQuestions: async () => {
+      const service = new QuestionsService();
+      const questions = await service.fetchQuestions();
 
-          set({
-            questions: questions.data.data,
-          });
-        },
-        fetchQuestionsFromFolder: async (folderId: number) => {
-          const service = new QuestionsService();
-          const questions = await service.fetchQuestionsFromFolder(folderId);
+      set({
+        questions: questions.data.data,
+      });
+    },
+    fetchQuestionsFromFolder: async (folderId: number) => {
+      const service = new QuestionsService();
+      const questions = await service.fetchQuestionsFromFolder(folderId);
 
-          set({
-            questions: questions.data.data,
-          });
-        },
-      }),
-      {
-        name: ICookieAuthKeys.questions,
-      },
-    ),
-  ),
+      set({
+        questions: questions.data.data,
+      });
+    },
+  })),
 );
